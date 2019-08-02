c889945b08604ab93c93d736b1233c5b71ba81c1
Liana Norman <liananorman@Lianas-MacBook-Pro.local>
2019-08-02 14:34:13 -0400

    adds finished app

--- MVC Lab App/RandomColor.swift
+++ MVC Lab App/RandomColor.swift
@@ -23,8 +23,25 @@ func randomColor() -> UIColor {
                    alpha: 1.0)
 }
 
-    func findBiggestNumber() -> RGB {
-        
+    func findBiggestNumber() -> CGFloat {
+       let arrOfColors = [red, green, blue]
+        let filteredArr = arrOfColors.sorted(by: {$0 > $1})
+        return filteredArr[0]
+    }
+    
+    func highestColor() -> UIColor {
+        let arrOfColors = [red, green, blue]
+        let filteredArr = arrOfColors.sorted(by: {$0 > $1})
+        switch filteredArr[0] {
+        case red :
+            return UIColor.red
+        case blue :
+            return UIColor.blue
+        case green :
+            return UIColor.green
+        default :
+            return UIColor.clear
+        }
     }
 }
 
--- MVC Lab App/ViewController.swift
+++ MVC Lab App/ViewController.swift
@@ -11,18 +11,88 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var background: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var gameOverLabel: UILabel!
+    @IBOutlet weak var newGameLabel: UIButton!
+    @IBOutlet weak var highScoreLabel: UILabel!
     
-    @IBAction func generatesRandColor(_ sender: UIButton) {
-        let random = RGB()
-        background.backgroundColor = random.randomColor()
-    }
-    
+    var model = RGB()
+    var score = 0
+    var highScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        generateRandomColor()
+        gameOverLabel.isHidden = true
+        newGameLabel.isHidden = true
         // Do any additional setup after loading the view.
     }
+    
+    func updateScore() {
+        score += 1
+        label.text = score.description
+    }
+    
+    func updateHighScore() {
+        if score > highScore {
+            highScoreLabel.text = score.description
+        } else {
+            highScoreLabel.text = highScore.description
+        }
+    }
 
+    func generateRandomColor() {
+       model = RGB()
+        background.backgroundColor = model.randomColor()
+    }
+    
+    func resetGame() {
+        label.text = score.description
+        gameOverLabel.isHidden = false
+        newGameLabel.isHidden = false
+    }
+    
+    @IBAction func generatesRandColor(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            if model.highestColor() == UIColor.red {
+                updateScore()
+                generateRandomColor()
+            } else {
+                resetGame()
+                updateHighScore()
+                
+            }
+        case 1:
+            if model.highestColor() == UIColor.green {
+                updateScore()
+                generateRandomColor()
+            } else {
+                resetGame()
+                updateHighScore()
+            }
+        case 2:
+            if model.highestColor() == UIColor.blue {
+                updateScore()
+                generateRandomColor()
+            } else {
+                resetGame()
+                updateHighScore()
+            }
+        case 3:
+            score = 0
+            resetGame()
+            generateRandomColor()
+            gameOverLabel.isHidden = true
+            newGameLabel.isHidden = true
+        default:
+            print ("something is wrong")
+        }
+       
+    }
+    
+    
+    
 
 }
 
